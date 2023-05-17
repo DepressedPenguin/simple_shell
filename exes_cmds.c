a1431da64597f2d460aad8fef9bff02fecd83786
ZAKARIA <zakariaelaro@gmail.com>
2023-05-17 10:27:56 -0700

    add comments & betty

--- exes_cmds.c
+++ exes_cmds.c
@@ -1,26 +1,51 @@
 #include "myshell.h"
-void exes_cmds(char** args) {
-    char *cmd_path;
-    pid_t pds = fork();
-    if (pds == 0) {
-        if (args[0][0] == '/') {
-            execve(args[0], args, NULL);
-        } else {
-           cmd_path = find_wayy(args[0]);
 
-            if (cmd_path == 0) {
-                printf("%s: No such file or directory\n",  exe_name_cmd[0]);
-                exit(1);
-            }
+/*
+ * exes_cmds - run commands with the specified parameters
+ *
+ * @args: command and its arguments are contained in an array of strings.
+ */
+void exes_cmds(char **args)
+{
+char *cmd_path;
+pid_t pds = fork();
 
-            execve(cmd_path, args, NULL);
-        }
-        printf("%s: failed to execute\n", args[0]);
-        exit(1);
-    } else if (pds > 0) {
-        wait(NULL);
-    } else {
-        exit(1);
-    }
+if (pds == 0)
+{
+	/* check if command has an absolute path */
+if (args[0][0] == '/')
+{
+execve(args[0], args, NULL);
+}
+else
+{
+	/* search for command in directories listed in PATH */
+cmd_path = find_wayy(args[0]);
+
+if (cmd_path == 0)
+{
+printf("%s: No such file or directory\n", exe_name_cmd[0]);
+exit(1);
+}
+
+execve(cmd_path, args, NULL);
+}
+
+	/* print error message if command execution fails */
+printf("%s: failed to execute\n", args[0]);
+exit(1);
+
+}
+else if (pds > 0)
+{
+	/* wait for child process to complete */
+wait(NULL);
+
+}
+else
+{
+	/* exit if fork() failed */
+exit(1);
+}
 }
 
--- read_stdin.c
+++ read_stdin.c
@@ -1,56 +1,79 @@
 #include "myshell.h"
-char **read_stdin(char *input) {
-    char **argz = malloc(sizeof(char*) * MAX_ARZ);
-    char *toksign;
-    int v = 0;
 
-    if (argz == 0) {
-        return (NULL);
-    }
+/*
+ * read_stdin - Tokenizes the input string and stores tokens.
+ * @input: The input string to tokenize
+ *
+ * Return: An array of strings containing the tokens, or NULL on failure
+ */
+char **read_stdin(char *input)
+{
+char **argz = malloc(sizeof(char *) * MAX_ARZ);
+char *toksign;
+int v = 0;
 
-    toksign = strtok(input, " ");
-    
-    for ( v = 0 ; toksign != 0 ; v++) {
-        if (v >= MAX_ARZ - 1) {
-            return (NULL);
-        }
-
-        argz[v] = malloc(sizeof(char) * MAXA_LEN);
-        if (argz[v] == 0) {
-             return (NULL);
-        }
-
-        strcpy(argz[v], toksign);
-
-        toksign = strtok(NULL, " ");
-    }
-
-    argz[v] = NULL;
-
-    return argz;
+if (argz == 0)
+	{
+return (NULL);
 }
 
-char* find_wayy(char* commands) {
-    char *wayy = getenv("PATH");
-    char *dr;
-    char *fulls_canal = malloc(sizeof(char) * (MAXC_LEN + 1));
+toksign = strtok(input, " ");
 
-    if (fulls_canal == 0) {
-        return (NULL);
-    }
-
-    dr = strtok(wayy, ":");
-    while (dr != 0) {
-        snprintf(fulls_canal, MAXC_LEN, "%s/%s", dr, commands);
-
-        if (access(fulls_canal, X_OK) == 0) {
-            return fulls_canal;
-        }
-
-        dr = strtok(NULL, ":");
-    }
-
-    free(fulls_canal);
-
-    return (NULL);
+for (v = 0; toksign != 0; v++)
+{
+if (v >= MAX_ARZ - 1)
+{
+return (NULL);
 }
+
+argz[v] = malloc(sizeof(char) * MAXA_LEN);
+if (argz[v] == 0)
+{
+return (NULL);
+}
+
+strcpy(argz[v], toksign);
+
+toksign = strtok(NULL, " ");
+}
+
+argz[v] = NULL;
+
+return (argz);
+}
+
+/*
+ * find_wayy - Searches for the specified command.
+ * @commands: The command to search for
+ *
+ * Return: The full path of the command if found, NULL otherwise
+ */
+char *find_wayy(char *commands)
+{
+char *wayy = getenv("PATH");
+char *dr;
+char *fulls_canal = malloc(sizeof(char) * (MAXC_LEN + 1));
+
+if (fulls_canal == 0)
+{
+return (NULL);
+}
+
+dr = strtok(wayy, ":");
+while (dr != 0)
+{
+snprintf(fulls_canal, MAXC_LEN, "%s/%s", dr, commands);
+
+if (access(fulls_canal, X_OK) == 0)
+{
+return (fulls_canal);
+}
+
+dr = strtok(NULL, ":");
+}
+
+free(fulls_canal);
+
+return (NULL);
+}
+

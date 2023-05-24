6db58b99d318d6cfc849638bc72e1a49848031a7
ZAKARIA <zakariaelaro@gmail.com>
2023-05-24 14:48:24 -0700

    check this code for the last time

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+`#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -7,17 +7,12 @@
 
 #define MAX_COMMAND_LENGTH 1024
 #define MAX_ARGS 64
-extern char **environ;
 
 void tokenizeCommand(char* command, char*** args, int* argCount) {
     char* token;
     int argIndex = 0;
-    
+
     token = strtok(command, " \t\n");
-    if (token == NULL) {
-        *argCount = 0;
-        return;
-    }
     while (token != NULL && argIndex < MAX_ARGS - 1) {
         (*args)[argIndex] = strdup(token);
         argIndex++;
@@ -39,32 +34,13 @@ void executeCommand(char** args, const char* executableName, int commandNumber) 
     pid_t pid;
     int status;
 
-    // Check if the command exists in the PATH
-    char* path = getenv("PATH");
-    char* pathEntry;
-    char commandPath[MAX_COMMAND_LENGTH];
-    int commandFound = 0;
-
-    while ((pathEntry = strsep(&path, ":")) != NULL) {
-        snprintf(commandPath, sizeof(commandPath), "%s/%s", pathEntry, args[0]);
-        if (access(commandPath, X_OK) == 0) {
-            commandFound = 1;
-            break;
-        }
-    }
-
-    if (!commandFound) {
-        fprintf(stderr, "%s: %d: %s: command not found\n", executableName, commandNumber, args[0]);
-        return;
-    }
-
     pid = fork();
     if (pid < 0) {
         perror("Fork failed");
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
-        if (execve(commandPath, args, environ) == -1) {
-            perror("Execve failed");
+        if (execvp(args[0], args) < 0) {
+            fprintf(stderr, "%s: %d: %s: not found\n", executableName, commandNumber, args[0]);
             exit(EXIT_FAILURE);
         }
     } else {
@@ -78,27 +54,12 @@ void executeCommand(char** args, const char* executableName, int commandNumber) 
     }
 }
 
-int changeDirectory(char** args, int argCount) {
-    if (argCount != 2) {
-        fprintf(stderr, "cd: invalid number of arguments\n");
-        return 1;
-    }
-
-    if (chdir(args[1]) != 0) {
-        perror("cd failed");
-        return 1;
-    }
-
-    return 0;
-}
-
 int main(int argc, char *argv[]) {
-    char* command = NULL;
+    char command[MAX_COMMAND_LENGTH];
     char** args = NULL;
     int argCount = 0;
     int commandNumber = 0;
-    ssize_t lineSize;
-    size_t bufferSize = 0;
+
     FILE *inputStream = stdin;
     int interactiveMode = 1;
 
@@ -117,13 +78,12 @@ int main(int argc, char *argv[]) {
             fflush(stdout);
         }
 
-        bufferSize = 0;
-        lineSize = getline(&command, &bufferSize, inputStream);
-        if (lineSize == -1) {
+        if (fgets(command, sizeof(command), inputStream) == NULL) {
             printf("\n");
             break;
         }
-        command[lineSize - 1] = '\0';
+
+        command[strcspn(command, "\n")] = '\0';
 
         if (strcmp(command, "exit") == 0) {
             break;
@@ -138,16 +98,7 @@ int main(int argc, char *argv[]) {
         }
 
         tokenizeCommand(command, &args, &argCount);
-        if (argCount > 0) {
-            if (strcmp(args[0], "cd") == 0) {
-                if (changeDirectory(args, argCount) != 0) {
-                    fprintf(stderr, "Failed to change directory\n");
-                }
-            } else {
-                executeCommand(args, argv[0], commandNumber);
-            }
-        }
-
+        executeCommand(args, argv[0], commandNumber);
         freeArguments(args);
     }
 
@@ -155,8 +106,5 @@ int main(int argc, char *argv[]) {
         fclose(inputStream);
     }
 
-    free(command);
-
     return 0;
-}
-
+}`

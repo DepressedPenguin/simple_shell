405732fd39a64da17649c39967742a16ffa96ca7
ZAKARIA <zakariaelaro@gmail.com>
2023-05-24 04:44:30 -0700

    fix this code

--- shell.c
+++ shell.c
@@ -1,144 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAXC_LEN 1024
-#define MAX_ARZ 64
-#define MAXA_LEN 128
+#define MAXC_LENZ 1024
+#define MAX_ARGZ 64
+#define MAXA_LENZ 128
 
-char **exe_name_cmd;
+char **read_stdins(char *input) {
+    char **argz;
+    char *sign;
+    int x;
 
-char **read_stdin(char *input) {
-    char **argz = malloc(sizeof(char*) * MAX_ARZ);
-    char *toksign;
-    int v = 0;
-
-    if (argz == 0) {
-        return (NULL);
+    argz = (char **) malloc(sizeof(char*) * MAX_ARGZ);
+    if (argz == NULL) {
+        return NULL;
     }
 
-    toksign = strtok(input, " ");
-    
-    for ( v = 0 ; toksign != 0 ; v++) {
-        if (v >= MAX_ARZ - 1) {
-            return (NULL);
+    sign = strtok(input, " ");
+    for (x = 0; sign != NULL; x++) {
+        if (x >= MAX_ARGZ - 1) {
+            return NULL;
         }
 
-        argz[v] = malloc(sizeof(char) * MAXA_LEN);
-        if (argz[v] == 0) {
-             return (NULL);
+        argz[x] = (char*) malloc(sizeof(char) * MAXA_LENZ);
+        if (argz[x] == NULL) {
+            return NULL;
         }
 
-        strcpy(argz[v], toksign);
+        strcpy(argz[x], sign);
 
-        toksign = strtok(NULL, " ");
+        sign = strtok(NULL, " ");
     }
 
-    argz[v] = NULL;
+    argz[x] = NULL;
 
     return argz;
 }
 
-char* find_wayy(char* commands) {
-    char *wayy = getenv("PATH");
-    char *dr;
-    char *fulls_canal = malloc(sizeof(char) * (MAXC_LEN + 1));
-
-    if (fulls_canal == 0) {
-        return (NULL);
-    }
-
-    dr = strtok(wayy, ":");
-    while (dr != 0) {
-        snprintf(fulls_canal, MAXC_LEN, "%s/%s", dr, commands);
-
-        if (access(fulls_canal, X_OK) == 0) {
-            return fulls_canal;
-        }
-
-        dr = strtok(NULL, ":");
-    }
-
-    free(fulls_canal);
-
-    return (NULL);
-}
-
-void exes_cmds(char** args) {
-    pid_t pds = fork();
-    if (pds == 0) {
-        if (args[0][0] == '/') {
-            execve(args[0], args, NULL);
-        } else {
-            char *cmd_path = find_wayy(args[0]);
-
-            if (cmd_path == 0) {
-                printf("%s: No such file or directory\n",  exe_name_cmd[0]);
-                exit(1);
-            }
-
-            execve(cmd_path, args, NULL);
-        }
-        printf("%s: failed to execute\n", args[0]);
+void enforce_cmd(char** arjs) {
+    pid_t pids = fork();
+    if (pids == 0) {
+        char *envp[] = { NULL };
+        execve(arjs[0], arjs, envp);
+        printf("./shell: No such file or directory\n");
         exit(1);
-    } else if (pds > 0) {
+    } else if (pids > 0) {
         wait(NULL);
     } else {
+        printf("Failed to fork process\n");
         exit(1);
     }
 }
 
-
-void exit_myshell(char** args) {
-    if (strcmp(args[0], "exit") == 0) {
-    if (args[1] != NULL) {
-        int exit_status = atoi(args[1]);
-        exit(exit_status);
-    } else {
-        exit(0); 
-    }
-}
-
-    
-}
-
-void printing_env() {
-    extern char **environ;
-    char **env = environ;
-    while (*env) {
-        printf("%s\n", *env);
-        env++;
-    }
-}
-
-int main(__attribute__((unused))int argc, char* argv[]) {
-    char *stdput = NULL;
-    size_t stdput_leng = 0;
-    char **args;
-
-    exe_name_cmd = argv;
+int main() {
+    char *btn = NULL;
+    size_t stdinz_size = 0;
+    char **arjs;
+    int i;
 
     while (1) {
         printf("$ ");
-        getline(&stdput, &stdput_leng, stdin);
-        stdput[strcspn(stdput, "\n")] = 0;
+        getline(&btn, &stdinz_size, stdin);
 
-        args = read_stdin(stdput);
+        btn[strlen(btn)-1] = '\0';
 
-        if (args[0] != NULL) {
-            if (strcmp(args[0], "exit") == 0) {
-                exit_myshell(args);
-            } else if (strcmp(args[0], "env") == 0) {
-                printing_env();
-            } else {
-                exes_cmds(args);
-            }
+        arjs = read_stdins(btn);
+
+        enforce_cmd(arjs);
+
+        for (i = 0; arjs[i] != NULL; i++) {
+            free(arjs[i]);
         }
+        free(arjs);
     }
 
-    free(stdput);
+    free(btn);
     return 0;
 }

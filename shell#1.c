cf6cf89772d6cc95c97620d62f49135d2bcdd4db
ZAKARIA <zakariaelaro@gmail.com>
2023-05-24 02:40:30 -0700

    check the file

--- shell#1.c
+++ shell#1.c
@@ -6,53 +6,56 @@
 
 extern char **environ;
 
-int main(void)
-{
-char *command = NULL;
-size_t bufsize = 0;
+char* readCommand() {
+    char *command = NULL;
+    size_t bufsize = 0;
 
-while (1)
-{
-printf("#cisfun$ ");
-if (getline(&command, &bufsize, stdin) == -1)
-{
-if (feof(stdin))
-{
-printf("\n");
-break;
-}
-perror("getline");
-exit(EXIT_FAILURE);
-}
-if (command[strlen(command) - 1] == '\n')
-command[strlen(command) - 1] = '\0';
+    printf("#cisfun$ ");
+    if (getline(&command, &bufsize, stdin) == -1) {
+        if (feof(stdin)) {
+            printf("\n");
+            return NULL;
+        }
+        perror("getline");
+        exit(EXIT_FAILURE);
+    }
 
-if (access(command, X_OK) == 0) 
-{
-if (fork() == 0) 
-{     
-char **argv = malloc(2 * sizeof(char *));
-argv[0] = command;
-argv[1] = NULL;
+    if (command[strlen(command) - 1] == '\n')
+        command[strlen(command) - 1] = '\0';
 
-if (execve(command, argv, environ) == -1)
-{
-perror(command);
-exit(EXIT_FAILURE);
-}
-break;
-}
-else
-{
-wait(NULL);
-}
-} 
-else
-{
-fprintf(stderr, "%s: No such file or directory\n", command);
-}
+    return command;
 }
 
-free(command);
-return EXIT_SUCCESS;
+void executeCommand(char* command) {
+    if (access(command, X_OK) == 0) {
+        if (fork() == 0) {
+            char **argv = malloc(2 * sizeof(char *));
+            argv[0] = command;
+            argv[1] = NULL;
+
+            if (execve(command, argv, environ) == -1) {
+                perror(command);
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            wait(NULL);
+        }
+    } else {
+        fprintf(stderr, "%s: No such file or directory\n", command);
+    }
 }
+
+int main(void) {
+    char *command;
+
+    while (1) {
+        command = readCommand();
+        if (command == NULL)
+            break;
+        executeCommand(command);
+    }
+
+    free(command);
+    return EXIT_SUCCESS;
+}
+

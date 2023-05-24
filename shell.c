cd478dd6dbfa196f9edfe74c60b793b5fae615dc
ZAKARIA <zakariaelaro@gmail.com>
2023-05-24 05:28:39 -0700

    check code v2

--- shell.c
+++ shell.c
@@ -4,87 +4,87 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAXC_LENZ 1024
-#define MAX_ARGZ 64
-#define MAXA_LENZ 128
+#define MAX_INPUT_LEN 1024
+#define MAX_ARGS 64
+#define MAX_ARG_LEN 128
 
 char **read_stdins(char *input) {
-    char **argz;
-    char *sign;
-    int x;
+    char **args;
+    char *token;
+    int argc = 0;
 
-    argz = (char **) malloc(sizeof(char*) * MAX_ARGZ);
-    if (argz == NULL) {
+    args = (char **) malloc(sizeof(char*) * MAX_ARGS);
+    if (args == NULL) {
+        perror("malloc");
         return NULL;
     }
 
-    sign = strtok(input, " ");
-    for (x = 0; sign != NULL; x++) {
-        if (x >= MAX_ARGZ - 1) {
+    token = strtok(input, " ");
+    while (token != NULL) {
+        if (argc >= MAX_ARGS - 1) {
+            fprintf(stderr, "Too many arguments\n");
             return NULL;
         }
 
-        argz[x] = (char*) malloc(sizeof(char) * MAXA_LENZ);
-        if (argz[x] == NULL) {
+        args[argc] = strdup(token);
+        if (args[argc] == NULL) {
+            perror("strdup");
             return NULL;
         }
 
-        strcpy(argz[x], sign);
-
-        sign = strtok(NULL, " ");
+        token = strtok(NULL, " ");
+        argc++;
     }
 
-    argz[x] = NULL;
+    args[argc] = NULL;
 
-    return argz;
+    return args;
 }
 
-void enforce_cmd(char** arjs) {
+void enforce_cmd(char **args) {
     pid_t pid = fork();
     if (pid == 0) {
-        execvp(arjs[0], arjs);
-        printf("./shell: Command not found: %s\n", arjs[0]);
+        execvp(args[0], args);
+        perror("execvp");
         exit(1);
     } else if (pid > 0) {
         wait(NULL);
     } else {
-        printf("Failed to fork process\n");
+        perror("fork");
         exit(1);
     }
 }
 
 int main() {
-    char *btn = NULL;
-    size_t stdinz_size = 0;
-    char **arjs;
+    char *input = NULL;
+    size_t input_size = 0;
+    char **args;
     int i;
 
     while (1) {
         printf("$ ");
-        if (getline(&btn, &stdinz_size, stdin) == -1) {
-            printf("Error reading input\n");
+        if (getline(&input, &input_size, stdin) == -1) {
+            perror("getline");
             break;
         }
 
-        if (btn[strlen(btn)-1] == '\n') {
-            btn[strlen(btn)-1] = '\0';
-        }
+        input[strcspn(input, "\n")] = '\0';
 
-        arjs = read_stdins(btn);
-        if (arjs == NULL) {
-            printf("Failed to parse input\n");
+        args = read_stdins(input);
+        if (args == NULL) {
+            fprintf(stderr, "Invalid input\n");
             continue;
         }
 
-        enforce_cmd(arjs);
+        enforce_cmd(args);
 
-        for (i = 0; arjs[i] != NULL; i++) {
-            free(arjs[i]);
+        for (i = 0; args[i] != NULL; i++) {
+            free(args[i]);
         }
-        free(arjs);
+        free(args);
     }
 
-    free(btn);
+    free(input);
     return 0;
 }
 

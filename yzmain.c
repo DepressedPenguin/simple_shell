8e8ecd78eac08150b3ca3989488fafbe17f43184
ZAKARIA <zakariaelaro@gmail.com>
2023-05-25 11:48:39 -0700

    betty style done!

--- yzmain.c
+++ yzmain.c
@@ -1,8 +1,13 @@
 #include "yzshell.h"
-
+/**
+ * main - Entry point of the yzshell program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing.
+ * Return: Always returns 0.
+ */
 int main(int argc __attribute__((unused)), char **argv)
 {
 zak = argv;
 run_mycode();
-return 0;
+return (0);
 }
